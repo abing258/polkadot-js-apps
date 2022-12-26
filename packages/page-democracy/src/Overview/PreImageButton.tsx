f10535eef6cc6d9b2b46bd7296ff37f202e6daf9
abing <410174833@qq.com>
2022-12-26 18:45:02 +0800

    init

---
+++ packages/page-democracy/src/Overview/PreImageButton.tsx
@@ -0,0 +1,42 @@
+// Copyright 2017-2022 @polkadot/app-democracy authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { Hash } from '@polkadot/types/interfaces';
+import type { HexString } from '@polkadot/util/types';
+
+import React from 'react';
+
+import { Button } from '@polkadot/react-components';
+import { useToggle } from '@polkadot/react-hooks';
+
+import { useTranslation } from '../translate';
+import PreImage from './PreImage';
+
+interface Props {
+  imageHash: Hash | HexString;
+  isImminent?: boolean;
+}
+
+function PreImageButton ({ imageHash, isImminent }: Props): React.ReactElement<Props> | null {
+  const { t } = useTranslation();
+  const [isPreimageOpen, togglePreimage] = useToggle();
+
+  return (
+    <>
+      <Button
+        icon='plus'
+        label={t<string>('Image')}
+        onClick={togglePreimage}
+      />
+      {isPreimageOpen && (
+        <PreImage
+          imageHash={imageHash}
+          isImminent={isImminent}
+          onClose={togglePreimage}
+        />
+      )}
+    </>
+  );
+}
+
+export default React.memo(PreImageButton);
